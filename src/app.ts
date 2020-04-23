cf7e5ff34513665ef3e323be85c26add9718ac1d
thir13en <santilpzate@gmail.com>
2020-04-23 15:14:12 +0200

    installed node types to test require

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
+const _ = require('lodash');
 import add from './add';
 
 
+const fruits = ['orange', 'strawberry', 'banana'];
 const number1 = 5;
 const number2 = 2.7;
+const first = _.first(fruits);
+console.log(first);
 
 const result = add(number1, number2);
 console.log(result);
