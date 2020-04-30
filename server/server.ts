081740e8fc05a98226de1be577245a128dd35398
thir13en <santilpzate@gmail.com>
2020-04-30 14:25:13 +0200

    connected to postgres database

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,24 @@
-import sequelize from 'sequelize';
+import { DataTypes, Model, Sequelize } from 'sequelize';
+
+
+const dbUrl = 'postgres://admin:secret@localhost:5432/ang-uni-typescript';
+const sequelize: Sequelize = new Sequelize(dbUrl);
+
+class Course extends Model {}
+Course.init({
+    description: DataTypes.STRING,
+    url: DataTypes.STRING,
+    longDescription: DataTypes.TEXT,
+    iconUrl: DataTypes.STRING,
+    courseListIcon: DataTypes.STRING,
+    seqNo: DataTypes.NUMBER,
+    comingSoon: DataTypes.BOOLEAN,
+    isNew: DataTypes.BOOLEAN,
+    isOngoing: DataTypes.BOOLEAN,
+}, { sequelize, modelName: 'Course' });
+
+Course.findAll()
+    .then(console.log);
 
 
 console.log('server is running!');
