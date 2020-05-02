d16505720c25e8e22288c33c6195a4f6b17fa984
thir13en <santilpzate@gmail.com>
2020-05-02 21:06:56 +0200

    successfully starting express server on 8090

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,12 @@
+import express from 'express';
+
 import { findAllCourses } from './queries';
 
+
+const app = express();
+
+app.listen(8090, () => {
+    console.log('successfully running!');
+})
 
 findAllCourses().then(res => console.log(JSON.stringify(res)));
