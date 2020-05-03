8c030a7276e5c8573ff259a437e0225428671359
thir13en <santilpzate@gmail.com>
2020-05-03 16:59:18 +0200

    preventend json hijacking by returning an object instead of an array

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ const app: Application = express();
 
 app.route('/api/courses')
     .get((req, res) => findAllCourses().then(
-        courses => res.status(200).json(courses),
+        courses => res.status(200).json({ courses }),
     ));
 
 app.listen(8090, () => {
