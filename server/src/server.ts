57d599d49de8237ea28f86e7805c672115f84ec0
thir13en <santilpzate@gmail.com>
2020-05-04 14:21:00 +0200

    started implementation of error handler

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,9 @@ const app: Application = express();
 
 initRestApi(app);
 
+// important to put after the api definition, since error handler must only trigger when api fails
+app.use((err, req, res) => console.log('err'));
+
 // start server
 app.listen(8090, () => {
     console.log('successfully running!');
