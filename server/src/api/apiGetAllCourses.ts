7be317f5f5f7e886a0d56e096b380ca27452f0ac
thir13en <santilpzate@gmail.com>
2020-05-05 19:12:26 +0200

    adding asynchronous error handling

--- server/src/api/apiGetAllCourses.ts
+++ server/src/api/apiGetAllCourses.ts
@@ -3,8 +3,10 @@ import { Request, Response } from 'express';
 import { findAllCourses } from '../queries';
 
 
-const apiGetAllCourses = (req: Request, res: Response) => findAllCourses().then(
-    courses => res.status(200).json({ courses }),
-);
+const apiGetAllCourses = (req: Request, res: Response) => findAllCourses()
+    .then(
+        courses => res.status(200).json({ courses }),
+    )
+    .catch(err => res.status(500).send());
 
 export default apiGetAllCourses;
