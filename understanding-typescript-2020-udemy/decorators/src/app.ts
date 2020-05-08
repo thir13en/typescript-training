8712a9e795aac6d4fb883cd96575959b33ca632c
thir13en <santilpzate@gmail.com>
2020-05-08 15:50:01 +0200

    created template injector decorator

--- understanding-typescript-2020-udemy/decorators/src/app.ts
+++ understanding-typescript-2020-udemy/decorators/src/app.ts
@@ -31,3 +31,24 @@ class ElocuentGenious {
         console.log('my name is ' + this.name)
     }
 }
+
+// an example of a decorator that injects template
+function WithTemplate(template: string, hookId: string): (constructor: Function) => any {
+    // we can use underscore when we don't need to access the underlying class
+    return function (_: Function) {
+        const hookEl = document.getElementById(hookId);
+
+        if (hookEl) {
+            hookEl.innerHTML = template;
+        }
+    }
+}
+
+@WithTemplate('<h1>hello world</h1>', 'hooked')
+class Injected {
+    name!: 'Santi';
+
+    constructor() {
+        console.log('my name is ' + this.name)
+    }
+}
