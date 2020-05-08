7e42a2550ae6f5b12047e00a2b8047203bb5a4a6
thir13en <santilpzate@gmail.com>
2020-05-08 15:37:24 +0200

    small comment

--- server/src/models/generateAssociations.ts
+++ server/src/models/generateAssociations.ts
@@ -1,7 +1,7 @@
 import LessonModel from './LessonModel';
 import CourseModel from './CourseModel';
 
-
+// if included associations in the models, some of them where undefined at runtime
 export default function generateAssociations() {
     // Course / Lesson
     CourseModel.hasMany(LessonModel, { foreignKey: 'courseId' });
