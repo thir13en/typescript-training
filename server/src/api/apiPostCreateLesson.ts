2350e3cd0d68fcb16bf7fde415432f030e0801fa
thir13en <santilpzate@gmail.com>
2020-05-09 22:30:06 +0200

    creating and testing post lesson

--- server/src/api/apiPostCreateLesson.ts
+++ server/src/api/apiPostCreateLesson.ts
@@ -6,8 +6,10 @@ import onError from './onError';
 import { createLesson } from '../queries';
 
 
+// TODO: create a post in znippets
 export default function apiPostCreateLesson(req: Request, res: Response) {
     createLesson(req.body)
         .then(partial(onSuccess, res))
+        .catch(console.error)
         .catch(partial(onError, res, 'find course detail courses failed'));
 }
