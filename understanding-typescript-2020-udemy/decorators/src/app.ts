f9aef18321854f3ffbbd1f299ea780d94c785caa
thir13en <santilpzate@gmail.com>
2020-05-29 14:23:22 +0200

    implemented a naive validator

--- understanding-typescript-2020-udemy/decorators/src/app.ts
+++ understanding-typescript-2020-udemy/decorators/src/app.ts
@@ -165,10 +165,50 @@ class Person {
     }
 }
 
+interface ValidatorConfig {
+    [property: string]: {
+        [validatableProp: string]: string[];
+    }
+}
 
-function Required() {}
-function PositiveNumber() {}
-function Validate(obj: object) {}
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+        ...registeredValidators[target.constructor.name],
+        [propName]: ['required'],
+    }
+}
+function PositiveNumber(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+        ...registeredValidators[target.constructor.name],
+        [propName]: ['required'],
+    }
+}
+function Validate(obj: any) {
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+
+    if (!objValidatorConfig) {
+        return true;
+    }
+
+    let isValid = true;
+    for (const prop in objValidatorConfig) {
+        for (const validator in objValidatorConfig[prop]) {
+            switch (validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop];
+                    break;
+                case 'positive':
+                    isValid = isValid && obj[prop] > 0;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    return isValid;
+}
 
 class Course {
     name: string;
