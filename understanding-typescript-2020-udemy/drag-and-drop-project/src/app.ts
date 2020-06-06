0b886499520355d0f7feded21b89c820cc2d8372
thir13en <santilpzate@gmail.com>
2020-06-06 17:15:21 +0200

    rendered form element from html template

--- understanding-typescript-2020-udemy/drag-and-drop-project/src/app.ts
+++ understanding-typescript-2020-udemy/drag-and-drop-project/src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+class ProjectInput {
+    private templateEl: HTMLTemplateElement;
+    private hostEl: HTMLDivElement;
+    readonly el: HTMLFormElement;
+
+    constructor() {
+        this.templateEl = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostEl = document.getElementById('app')! as HTMLDivElement;
+
+        const importedHTMLContent = document.importNode(this.templateEl.content, true);
+        this.el = importedHTMLContent.firstElementChild as HTMLFormElement;
+        this.attach(this.el);
+    }
+
+    private attach(el: HTMLFormElement) {
+        this.hostEl.insertAdjacentElement('afterbegin', el);
+    }
+}
+
+const projectInput = new ProjectInput();
