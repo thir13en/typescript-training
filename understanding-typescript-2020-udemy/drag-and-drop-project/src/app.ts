2517090edbfad406511a86c394c50276646c9420
thir13en <santilpzate@gmail.com>
2020-06-08 14:16:37 +0200

    added typescript watch mode script and start working on the form

--- understanding-typescript-2020-udemy/drag-and-drop-project/src/app.ts
+++ understanding-typescript-2020-udemy/drag-and-drop-project/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     private templateEl: HTMLTemplateElement;
     private hostEl: HTMLDivElement;
     readonly el: HTMLFormElement;
+    private titleElField: HTMLInputElement;
+    // private descriptionElField: HTMLInputElement;
+    // private peopleElField: HTMLInputElement;
 
     constructor() {
         this.templateEl = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,7 +12,22 @@ class ProjectInput {
 
         const importedHTMLContent = document.importNode(this.templateEl.content, true);
         this.el = importedHTMLContent.firstElementChild as HTMLFormElement;
+        this.el.id = 'user-input';
+        this.titleElField = this.el.querySelector('#title')! as HTMLInputElement;
+        // this.descriptionElField = this.el.querySelector('#description')! as HTMLInputElement;
+        // this.peopleElField = this.el.querySelector('#people')! as HTMLInputElement;
+        this.configure(this.el);
         this.attach(this.el);
+    }
+
+    private submitHandler(ev: Event) {
+        ev.preventDefault();
+        console.log(this.titleElField.value);
+    }
+
+    private configure(el: HTMLFormElement) {
+        el.addEventListener('submit', this.submitHandler.bind(this));
+        debugger;
     }
 
     private attach(el: HTMLFormElement) {
