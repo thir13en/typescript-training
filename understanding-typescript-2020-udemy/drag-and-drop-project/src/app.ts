ce632de963b206f6e2bb0f09fa2d3e09a6605baf
thir13en <santilpzate@gmail.com>
2020-06-12 15:11:07 +0200

    added form submitting with validation

--- understanding-typescript-2020-udemy/drag-and-drop-project/src/app.ts
+++ understanding-typescript-2020-udemy/drag-and-drop-project/src/app.ts
@@ -1,11 +1,10 @@
-// autobing decorator
+// autobind decorator
 function Autobind(_: any, _2: string | Symbol, propDescriptor: PropertyDescriptor) {
     const originalMethod = propDescriptor.value;
     const adjustedDescriptor: PropertyDescriptor = {
         configurable: true,
         get() {
-            const boundFn = originalMethod.bind(this);
-            return boundFn;
+            return originalMethod.bind(this);
         }
     }
     return adjustedDescriptor;
@@ -16,8 +15,8 @@ class ProjectInput {
     private hostEl: HTMLDivElement;
     readonly el: HTMLFormElement;
     private titleElField: HTMLInputElement;
-    // private descriptionElField: HTMLInputElement;
-    // private peopleElField: HTMLInputElement;
+    private descriptionElField: HTMLInputElement;
+    private peopleElField: HTMLInputElement;
 
     constructor() {
         this.templateEl = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -27,16 +26,37 @@ class ProjectInput {
         this.el = importedHTMLContent.firstElementChild as HTMLFormElement;
         this.el.id = 'user-input';
         this.titleElField = this.el.querySelector('#title')! as HTMLInputElement;
-        // this.descriptionElField = this.el.querySelector('#description')! as HTMLInputElement;
-        // this.peopleElField = this.el.querySelector('#people')! as HTMLInputElement;
+        this.descriptionElField = this.el.querySelector('#description')! as HTMLInputElement;
+        this.peopleElField = this.el.querySelector('#people')! as HTMLInputElement;
         this.configure(this.el);
         this.attach(this.el);
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitleValue = this.titleElField.value;
+        const enteredDescriptionValue = this.descriptionElField.value;
+        const enteredPeopleValue = this.peopleElField.value;
+
+        if (
+            enteredTitleValue.trim().length === 0 ||
+            enteredDescriptionValue.trim().length === 0 ||
+            enteredPeopleValue.trim().length === 0
+        ) {
+            alert('You need to fill all fields');
+            return;
+        } else {
+            return [enteredTitleValue, enteredDescriptionValue, +enteredPeopleValue];
+        }
     }
 
     @Autobind
     private submitHandler(ev: Event) {
         ev.preventDefault();
-        console.log(this.titleElField.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+        }
     }
 
     private configure(el: HTMLFormElement) {
