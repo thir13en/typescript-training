abe24eb2c7021abc5920ffc53992ba799edc76d6
thir13en <santilpzate@gmail.com>
2020-07-04 21:31:11 +0200

    created project for setting up with webpack

---
+++ understanding-typescript-2020-udemy/webpack-setup/src/app.ts
@@ -0,0 +1,7 @@
+import { ProjectInput } from './components/project-input.js';
+import { ProjectsList } from './components/projects-list.js';
+
+
+new ProjectInput();
+new ProjectsList('active');
+new ProjectsList('finished');
