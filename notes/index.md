19ee6b62b5ccce88f778aaf554271b726938d8c1
thir13en <santilpzate@gmail.com>
2020-07-16 07:00:52 +0200

    gathering user input

--- notes/index.md
+++ notes/index.md
@@ -1,5 +1,5 @@
 # Table of contents.
 
-1. [Code Splitting](./CODE-SLITTING.md)
+1. [Code Splitting](code-splitting.md)
 1. [Imports](./imports.md)
 1. [Enums](./enums.md)
--- understanding-typescript-2020-udemy/share-place/src/app.ts
+++ understanding-typescript-2020-udemy/share-place/src/app.ts
@@ -1,1 +1,9 @@
-// Code goes here!
+const form  = document.querySelector('form')!;
+const addrInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addrInput.value;
+}
+
+form.addEventListener('submit', searchAddressHandler);
