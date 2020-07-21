91819de94866f6082880721d2d95a1330c5c9e96
santiloopz <santiloopz@localhost.localdomain>
2020-07-21 04:20:41 +0200

    working with google maps api

--- understanding-typescript-2020-udemy/share-place/src/app.ts
+++ understanding-typescript-2020-udemy/share-place/src/app.ts
@@ -1,6 +1,8 @@
 const form  = document.querySelector('form')!;
 const addrInput = document.getElementById('address')! as HTMLInputElement;
 
+const GOOGLE_API_KEY = 'AIzaSyBK-TpU34k-7qt9eHp_JdhnU_Krgi5sLEA';
+
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAddress = addrInput.value;
