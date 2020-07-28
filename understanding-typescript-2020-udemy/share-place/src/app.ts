c44bb1024cced2366abf5589878ff21b8f95b8a2
santiloopz <santilpzate@gmail.com>
2020-07-28 08:18:42 +0200

    creating place request url

--- understanding-typescript-2020-udemy/share-place/src/app.ts
+++ understanding-typescript-2020-udemy/share-place/src/app.ts
@@ -1,3 +1,6 @@
+import axios from 'axios';
+
+
 const form  = document.querySelector('form')!;
 const addrInput = document.getElementById('address')! as HTMLInputElement;
 
@@ -6,6 +9,8 @@ const GOOGLE_API_KEY = 'AIzaSyBK-TpU34k-7qt9eHp_JdhnU_Krgi5sLEA';
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAddress = addrInput.value;
+
+    axios.get();
 }
 
 form.addEventListener('submit', searchAddressHandler);
