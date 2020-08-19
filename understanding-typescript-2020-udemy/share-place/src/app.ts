5746037f228aa52be16ec7f79cde6d2f7533f47e
santiloopz <santilpzate@gmail.com>
2020-08-19 09:51:52 +0200

    added to do to activate billing account

--- understanding-typescript-2020-udemy/share-place/src/app.ts
+++ understanding-typescript-2020-udemy/share-place/src/app.ts
@@ -4,15 +4,13 @@ import axios from 'axios';
 const form  = document.querySelector('form')!;
 // const addrInput = document.getElementById('address')! as HTMLInputElement;
 
-// TODO figure out how to obtain environment variables in this execution context
-console.log(process.env.TS_TEST);
-
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     // const enteredAddress = addrInput.value;
+    // TODO: activate billing account
 
-    // TODO: add notes on geocoding Google API
-    axios.get('');
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${process.env.GOOGLE_API_KEY}`)
+        .then(console.log);
 }
 
 form.addEventListener('submit', searchAddressHandler);
